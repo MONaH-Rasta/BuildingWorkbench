f4a2257d8e54b75617d8d080375e44a0abbf59ac
MON@H <MONaH.Rasta@gmail.com>
2024-05-06 19:33:52 +0300

    v1.0.2

---
+++ BuildingWorkbench.cs
@@ -0,0 +1,392 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Newtonsoft.Json;
+using Oxide.Core.Plugins;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("Building Workbench", "MJSU", "1.0.2")]
+    [Description("Extends the range of the workbench to work inside the entire building")]
+    public class BuildingWorkbench : RustPlugin
+    {
+        #region Class Fields
+        [PluginReference] private readonly Plugin GameTipAPI;
+
+        private PluginConfig _pluginConfig; //Plugin Config
+
+        private TriggerBase _triggerBase;
+        private GameObject _object;
+
+        private const string UsePermission = "buildingworkbench.use";
+        private const string AccentColor = "#de8732";
+
+        private readonly List<ulong> _notifiedPlayer = new List<ulong>();
+        private readonly Hash<uint, BuildingData> _buildingData = new Hash<uint, BuildingData>();
+
+        private static BuildingWorkbench _ins;
+
+        private bool _init;
+        #endregion
+
+        #region Setup & Loading
+        private void Init()
+        {
+            _ins = this;
+            permission.RegisterPermission(UsePermission, this);
+
+            _object = new GameObject();
+            _triggerBase = _object.AddComponent<TriggerBase>();
+        }
+        
+        protected override void LoadDefaultMessages()
+        {
+            lang.RegisterMessages(new Dictionary<string, string>
+            {
+                [LangKeys.Chat] = $"<color=#bebebe>[<color={AccentColor}>{Title}</color>] {{0}}</color>",
+                [LangKeys.Notification] = "Your workbench range has been increased to work inside your building"
+            }, this);
+        }
+        
+        protected override void LoadDefaultConfig()
+        {
+            PrintWarning("Loading Default Config");
+        }
+
+        protected override void LoadConfig()
+        {
+            base.LoadConfig();
+            Config.Settings.DefaultValueHandling = DefaultValueHandling.Populate;
+            _pluginConfig = Config.ReadObject<PluginConfig>();
+            Config.WriteObject(_pluginConfig);
+        }
+
+        private void OnServerInitialized()
+        {
+            foreach (Workbench workbench in GameObject.FindObjectsOfType<Workbench>())
+            {
+                AddWorkbench(workbench);
+            }
+            
+            foreach (BasePlayer player in BasePlayer.activePlayerList)
+            {
+                OnPlayerInit(player);
+            }
+            
+            _init = true;
+        }
+
+        private void OnPlayerInit(BasePlayer player)
+        {
+            if (!HasPermission(player, UsePermission))
+            {
+                return;
+            }
+
+            if (player.GetComponent<WorkbenchBehavior>() != null)
+            {
+                return;
+            }
+
+            player.EnterTrigger(_triggerBase);
+            player.gameObject.AddComponent<WorkbenchBehavior>();
+        }
+
+        private void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            player.gameObject.GetComponent<WorkbenchBehavior>()?.DoDestroy();
+        }
+
+        private void Unload()
+        {
+            foreach (WorkbenchBehavior behavior in GameObject.FindObjectsOfType<WorkbenchBehavior>())
+            {
+                behavior.DoDestroy();
+            }
+            GameObject.Destroy(_object);
+            _ins = null;
+        }
+        #endregion
+
+        #region Oxide Hooks
+        private void OnEntityLeave(TriggerBase trigger, BaseEntity entity)
+        {
+            BasePlayer player = entity.ToPlayer();
+            if (player != null && trigger == _triggerBase)
+            {
+                player.EnterTrigger(_triggerBase);
+            }
+        }
+
+        private void OnEntitySpawned(Workbench bench)
+        {
+            if (!_init)
+            {
+                return;
+            }
+
+            BasePlayer player = BasePlayer.FindByID(bench.OwnerID);
+            if (player == null)
+            {
+                return;
+            }
+
+            AddWorkbench(bench);
+            
+            List<WorkbenchBehavior> behaviors = GetBuildingData(bench.buildingID).Behaviors;
+            for (int i = behaviors.Count - 1; i >= 0; i--)
+            {
+                behaviors[i].OnWorkbenchChanged();
+            }
+
+            WorkbenchBehavior benchBehavior = player.GetComponent<WorkbenchBehavior>();
+            if (benchBehavior == null)
+            {
+                return;
+            }
+            
+            benchBehavior.OnWorkbenchChanged();
+
+            if (!_pluginConfig.EnableNotifications)
+            {
+                return;
+            }
+
+            if (_notifiedPlayer.Contains(player.userID))
+            {
+                return;
+            }
+
+            _notifiedPlayer.Add(player.userID);
+
+            if (GameTipAPI == null)
+            {
+                Chat(player, Lang(LangKeys.Notification, player));
+            }
+            else
+            {
+                GameTipAPI.Call("ShowGameTip", player, Lang(LangKeys.Notification, player), 6f);
+            }
+        }
+
+        private void OnEntityKill(Workbench bench)
+        {
+            RemoveWorkbench(bench);
+        }
+        
+        private void OnCupboardAuthorize(BuildingPrivlidge privilege, BasePlayer player)
+        {
+            OnAuthChanged(player);
+        }
+        
+        private void OnCupboardDeauthorize(BuildingPrivlidge privilege, BasePlayer player)
+        {
+            OnAuthChanged(player);
+        }
+        
+        private void OnAuthChanged(BasePlayer player)
+        {
+            NextTick(() =>
+            {
+                player.GetComponent<WorkbenchBehavior>()?.OnAuthChanged();
+            });
+        }
+
+        private void OnCupboardClearList(BuildingPrivlidge privilege, BasePlayer player)
+        {
+            NextTick(() =>
+            {
+                player.GetComponent<WorkbenchBehavior>()?.OnAuthChanged();
+
+                List<WorkbenchBehavior> behaviors = GetBuildingData(privilege.buildingID).Behaviors;
+                for (int i = behaviors.Count - 1; i >= 0; i--)
+                {
+                    behaviors[i].OnAuthChanged();
+                }
+            });
+        }
+        #endregion
+
+        #region Behavior
+        private class WorkbenchBehavior : FacepunchBehaviour
+        {
+            private BasePlayer Player { get; set; }
+            private bool IsInsideBuildingPrivilege { get; set; }
+            private uint BuildingId { get; set; }
+            private bool ForceUpdate { get; set; }
+
+            private void Awake()
+            {
+                enabled = false;
+                Player = GetComponent<BasePlayer>();
+                InvokeRepeating(CheckInsideBuildingPrivilege, 1f, _ins._pluginConfig.InsideBuildingPrivilegeInterval);
+            }
+
+            public void OnAuthChanged()
+            {
+                CheckInsideBuildingPrivilege();
+                ForceUpdatePlayer();
+            }
+            
+            public void OnWorkbenchChanged()
+            {
+                ForceUpdatePlayer();
+            }
+
+            private void ForceUpdatePlayer()
+            {
+                ForceUpdate = true;
+                UpdatePlayerWorkbenchLevel();
+            }
+
+            private void CheckInsideBuildingPrivilege()
+            {
+                BuildingPrivlidge priv = Player.GetBuildingPrivilege();
+                bool isAuthed = priv?.IsAuthed(Player) ?? false;
+                
+                if (!IsInsideBuildingPrivilege && isAuthed)
+                {
+                    IsInsideBuildingPrivilege = true;
+                    BuildingId = priv.buildingID;
+                    _ins.AddBuilding(BuildingId, this);
+                    InvokeRandomized(UpdatePlayerWorkbenchLevel, 0, _ins._pluginConfig.UpdateWorkbenchLevelInterval, 0.1f);
+                    //_ins.Puts($" Activating {_workbenchLevel}");
+                }
+                else if (IsInsideBuildingPrivilege && !isAuthed)
+                {
+                    IsInsideBuildingPrivilege = false;
+                    _ins.RemoveBuilding(BuildingId, this);
+                    BuildingId = 0;
+                    CancelInvoke(UpdatePlayerWorkbenchLevel);
+                    ForceUpdatePlayer();
+                    //_ins.Puts("Deactivating");
+                }
+
+                if (isAuthed && priv.buildingID != BuildingId)
+                {
+                    _ins.RemoveBuilding(BuildingId, this);
+                    BuildingId = priv.buildingID;
+                    _ins.AddBuilding(BuildingId, this);
+                }
+            }
+
+            private int GetBuildingWorkbenchLevel()
+            {
+                if (BuildingId == 0)
+                {
+                    return 0;
+                }
+
+                BuildingData buildingBenches = _ins.GetBuildingData(BuildingId);
+                return buildingBenches.Workbenches.Count == 0 ? 0 : buildingBenches.Workbenches.Max(b => b.Workbenchlevel);
+            }
+
+            private void UpdatePlayerWorkbenchLevel()
+            {
+                int workbenchLevel = GetBuildingWorkbenchLevel();
+                if (!ForceUpdate && (workbenchLevel <= 0 || !IsInsideBuildingPrivilege))
+                {
+                    return;
+                }
+
+                //_ins.Puts("Updating Level");
+                Player.nextCheckTime = Time.realtimeSinceStartup + _ins._pluginConfig.UpdateWorkbenchLevelInterval + .5f;
+
+                Player.cachedCraftLevel = workbenchLevel;
+
+                Player.SetPlayerFlag(BasePlayer.PlayerFlags.Workbench1, workbenchLevel == 1);
+                Player.SetPlayerFlag(BasePlayer.PlayerFlags.Workbench2, workbenchLevel == 2);
+                Player.SetPlayerFlag(BasePlayer.PlayerFlags.Workbench3, workbenchLevel == 3);
+                ForceUpdate = false;
+            }
+
+            public void DoDestroy()
+            {
+                _ins._buildingData[BuildingId]?.Behaviors.Remove(this);
+                Destroy(this);
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+
+        private void AddWorkbench(Workbench bench)
+        {
+            BuildingData data = GetBuildingData(bench.buildingID);
+            data.Workbenches.Add(bench);
+        }
+
+        private void RemoveWorkbench(Workbench bench)
+        {
+            BuildingData data = GetBuildingData(bench.buildingID);
+            data.Workbenches.Remove(bench);
+            
+            foreach (WorkbenchBehavior behavior in data.Behaviors)
+            {
+                behavior.OnWorkbenchChanged();
+            }
+        }
+
+        private void AddBuilding(uint buildingId, WorkbenchBehavior behavior)
+        {
+            BuildingData data = GetBuildingData(buildingId);
+            data.Workbenches = BuildingManager.server.GetBuilding(buildingId).decayEntities.OfType<Workbench>().ToList();
+            data.Behaviors.Add(behavior);
+        }
+        
+        private void RemoveBuilding(uint buildingId, WorkbenchBehavior behavior)
+        {
+            BuildingData data = GetBuildingData(buildingId);
+            data.Behaviors.Remove(behavior);
+        }
+
+        private BuildingData GetBuildingData(uint buildingId)
+        {
+            BuildingData data = _buildingData[buildingId];
+            if (data == null)
+            {
+                data = new BuildingData();
+                _buildingData[buildingId] = data;
+            }
+
+            return data;
+        }
+        
+        private void Chat(BasePlayer player, string format, params object[] args) => PrintToChat(player, Lang(LangKeys.Chat, player, format), args);
+        
+        private bool HasPermission(BasePlayer player, string perm) => permission.UserHasPermission(player.UserIDString, perm);
+        
+        private string Lang(string key, BasePlayer player = null, params object[] args) => string.Format(lang.GetMessage(key, this, player?.UserIDString), args);
+        #endregion
+
+        #region Classes
+        private class PluginConfig
+        {
+            [DefaultValue(true)]
+            [JsonProperty(PropertyName = "Enable Notifications")]
+            public bool EnableNotifications { get; set; }
+            
+            [DefaultValue(3f)]
+            [JsonProperty(PropertyName = "Inside Building Privilege Interval")]
+            public float InsideBuildingPrivilegeInterval { get; set; }
+
+            [DefaultValue(3f)]
+            [JsonProperty(PropertyName = "Update Workbench Level Interval")]
+            public float UpdateWorkbenchLevelInterval { get; set; }
+        }
+        
+        private class LangKeys
+        {
+            public const string Chat = "Chat";
+            public const string Notification = "Notification";
+        }
+
+        private class BuildingData
+        {
+            public List<Workbench> Workbenches = new List<Workbench>();
+            public readonly List<WorkbenchBehavior> Behaviors = new List<WorkbenchBehavior>();
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # BuildingWorkbench
+
 Oxide plugin for Rust. Extends the range of the workbench to work inside the entire building.
+
+## About
+
+For players with the permission the range of workbench will be extended throughout the entire building.
+
+* Note: Building needs to have a tool cupboard in order to work.
+
+## Permission
+
+* `buildingworkbench.use` - Allows players to have their workbench extended throughout the entire building.
+  * After adding the permission you need to reload the plugin or have the players rejoin.
+* `buildingworkbench.cancelcraft` - Players with this permission will have their crafting canceled if their workbench level changes below the crafting level. All items will be returned.
+
+### Grant Example
+
+ `o.grant group default buildingworkbench.use` - will grant all players extended workbench.
+
+## Configuration
+
+```json
+{
+  "Display workbench built notification": true,
+  "Display cancel craft notification": true,
+  "Inside building check frequency (Seconds)": 3.0,
+  "Distance from base to be considered inside building (Meters)": 16.0,
+  "Required distance from last update (Meters)": 5.0
+}
+```
+
+## Localization
+
+```json
+{
+  "Chat": "<color=#bebebe>[<color=#de8732>Building Workbench</color>] {0}</color>",
+  "Notification": "Your workbench range has been increased to work inside your building",
+  "CraftCanceledV1": "Your workbench level has changed. Crafts that required a higher level have been cancelled."
+}
+```
